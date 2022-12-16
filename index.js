712bc0266d01159702082974b18f4719be0cecc0
RodrigoMSCTeixeira <rodrigoteixeira@sefaz.pi.gov.br>
2022-12-16 12:43:25 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const listItems = [
+  'Item 1',
+  'Item 2',
+  'Item 3',
+  'Item 4',
+  'Item 5',
+  'Item 6',
+  'Item 7',
+  'Item 8',
+  'Item 9',
+  'Item 10',
+  'Item 11',
+  'Item 12',
+  'Item 13',
+  'Item 14',
+  'Item 15',
+  'Item 16',
+  'Item 17',
+  'Item 18',
+  'Item 19',
+  'Item 20',
+  'Item 21',
+  'Item 22',
+]
+
+const listElement = document.getElementById('list')
+const paginationElement = document.getElementById('pagination')
+
+let currentPage = 1
+let rows = 5
+
+displayList = (items, wrapper, rowsPerPage, page) => {
+  wrapper.innerHTML = ''
+  page--
+
+  let start = rowsPerPage * page
+  let end = start + rowsPerPage
+  let paginatedItems = items.slice(start, end)
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i]
+    let itemElement = document.createElement('div')
+    itemElement.classList.add('item')
+    itemElement.innerText = item
+
+    wrapper.appendChild(itemElement)
+  }
+}
+
+setupPagination = (items, wrapper, rowsPerPage) => {
+  wrapper.innerHTML = ''
+  let pageCount = Math.ceil(items.length / rowsPerPage)
+
+  for (let i = 1; i < pageCount + 1; i++) {
+    let btn = paginationButton(i, items)
+    wrapper.appendChild(btn)
+  }
+}
+
+paginationButton = (page, items) => {
+  let button = document.createElement('button')
+  button.innerText = page
+
+  currentPage == page ? button.classList.add('active') : ''
+
+  button.addEventListener('click', () => {
+    currentPage = page
+
+    displayList(items, listElement, rows, currentPage)
+
+    let currentBtn = document.querySelector('.pagenumbers button.active')
+    currentBtn.classList.remove('active')
+    button.classList.add('active')
+  })
+
+  return button
+}
+
+displayList(listItems, listElement, rows, currentPage)
+setupPagination(listItems, paginationElement, rows)
